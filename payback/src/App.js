83912224b36894e411dc5792eb6fc26265c1a98a
Parker Nix <71665527+ParkerNix@users.noreply.github.com>
2022-11-14 07:44:50 -0500

    Locks, Auto calc.

--- payback/src/App.js
+++ payback/src/App.js
@@ -1,26 +1,114 @@
-import {useEffect, useState} from 'react'
+import {useEffect, useState} from 'react';
 
 function App() {
-  const [total, setTotal] = useState(100)
-  const [persona, setPersona] = useState(20)
-  const [personb, setPersonb] = useState(30)
-  const [personc, setPersonc] = useState(50)
-  const [personapercent, setPersonapercent] = useState(0.2)
-  const [personbpercent, setPersonbpercent] = useState(0.3)
-  const [personcpercent, setPersoncpercent] = useState(0.5)
-  const [ratiotext, setRatiotext] = useState("hey, your ratios are messed up!")
+  const [total, setTotal] = useState(100);
+  const [persona, setPersona] = useState(20);
+  const [personb, setPersonb] = useState(30);
+  const [personc, setPersonc] = useState(50);
+  const [personapercent, setPersonapercent] = useState(0.2);
+  const [personbpercent, setPersonbpercent] = useState(0.3);
+  const [personcpercent, setPersoncpercent] = useState(0.5);
+  const [personalocked, setPersonalocked] = useState(20);
+  const [personblocked, setPersonblocked] = useState(30);
+  const [personclocked, setPersonclocked] = useState(50);
+  const [personapercentlocked, setPersonapercentlocked] = useState(0.2);
+  const [personbpercentlocked, setPersonbpercentlocked] = useState(0.3);
+  const [personcpercentlocked, setPersoncpercentlocked] = useState(0.5);
+  const [lock1, setlock1] = useState("lock");
+  const [lock2, setlock2] = useState("lock");
+  const [lock3, setlock3] = useState("lock");
+  const [lock1enabled, setlock1enabled] = useState(false);
+  const [lock2enabled, setlock2enabled] = useState(false);
+  const [lock3enabled, setlock3enabled] = useState(false);
+  const [error, seterror] = useState("");
+  const Evenvalues = (variable, num) => {
+    if ( (lock1enabled === true && lock2enabled === true) || (lock1enabled === true && lock3enabled === true) || (lock2enabled === true && lock3enabled === true) ) {
+      return
+    }
+    if ( variable === "persona" ) {
+      let difference = total - num;
+      let newdollar = difference/2;
+      setPersonb(newdollar);
+      setPersonc(newdollar);
+      let newpercent = newdollar/total;
+      setPersonbpercent(newpercent);
+      setPersoncpercent(newpercent);
+      if ( lock2enabled === true ) {
+        setPersonb(personblocked);
+        setPersonbpercent(personbpercentlocked);
+        let newdollar = difference - personblocked;
+        setPersonc(newdollar);
+        let newpercent = newdollar/total;
+        setPersonapercent(newpercent);
+      }
+      if ( lock3enabled === true ) {
+        setPersonc(personclocked);
+        setPersoncpercent(personcpercentlocked);
+        let newdollar = difference - personclocked;
+        setPersonb(newdollar);
+        let newpercent = newdollar/total;
+        setPersonbpercent(newpercent);
+      }
+    }
+    if ( variable === "personb" ) {
+      let difference = total - num;
+      let newdollar = difference/2;
+      setPersona(newdollar);
+      setPersonc(newdollar);
+      let newpercent = newdollar/total;
+      setPersonapercent(newpercent);
+      setPersoncpercent(newpercent);
+      if ( lock1enabled === true ) {
+        setPersona(personalocked);
+        setPersonapercent(personapercentlocked);
+        let newdollar = difference - personalocked;
+        setPersonc(newdollar);
+        let newpercent = newdollar/total;
+        setPersoncpercent(newpercent);
+      }
+      if ( lock3enabled === true ) {
+        setPersonc(personclocked);
+        setPersoncpercent(personcpercentlocked);
+        let newdollar = difference - personclocked;
+        setPersona(newdollar);
+        let newpercent = newdollar/total;
+        setPersonapercent(newpercent);
+      }
+    }
+    if ( variable === "personc" ) {
+      let difference = total - num;
+      let newdollar = difference/2;
+      setPersona(newdollar);
+      setPersonb(newdollar);
+      let newpercent = newdollar/total;
+      setPersonapercent(newpercent);
+      setPersonbpercent(newpercent);
+      if ( lock1enabled === true ) {
+        setPersona(personalocked);
+        setPersonapercent(personapercentlocked);
+        let newdollar = difference - personalocked;
+        setPersonb(newdollar);
+        let newpercent = newdollar/total;
+        setPersonbpercent(newpercent);
+      }
+      if ( lock2enabled === true ) {
+        setPersonb(personblocked);
+        setPersonbpercent(personbpercentlocked);
+        let newdollar = difference - personblocked;
+        setPersona(newdollar);
+        let newpercent = newdollar/total;
+        setPersonapercent(newpercent);
+      }
+    }
+  }
   const isEven = () => {
-    const personapercenttest = persona/total
-    setPersonapercent(personapercenttest)
-    const personbpercenttest = personb/total
-    setPersonbpercent(personbpercenttest)
-    const personcpercenttest = personc/total
-    setPersoncpercent(personcpercenttest)
-    const totalPercent = personapercenttest+personbpercenttest+personcpercenttest;
-    if (1 === totalPercent) {
-      setRatiotext("looks good!")
-    } else {
-      setRatiotext("hey, your ratios are messed up!");
+
+    const totalPercent = personapercent+personbpercent+personcpercent;
+    if ( 1 !== totalPercent ) {
+      seterror("Please recheck your values, they do not add up to your total!");
+    }
+    if ( 1 === totalPercent ) {
+      seterror("");
     }
   }
   useEffect(() => {
@@ -68,10 +156,11 @@ function App() {
                   value={persona}
                   onChange={
                     event => {
-                      const personanew = parseFloat(event.target.value)
-                      setPersona(personanew)
-                      const personapercentnew = personanew / total
-                      setPersonapercent(personapercentnew)
+                      const personanew = parseFloat(event.target.value);
+                      setPersona(personanew);
+                      const personapercentnew = personanew / total;
+                      setPersonapercent(personapercentnew);
+                      Evenvalues("persona", personanew);
                     }
                   }
                 />
@@ -87,10 +176,11 @@ function App() {
                   value={personb}
                   onChange={
                     event => {
-                      const personbnew = parseFloat(event.target.value)
-                      setPersonb(personbnew)
-                      const personbpercentnew = personbnew / total
-                      setPersonbpercent(personbpercentnew)
+                      const personbnew = parseFloat(event.target.value);
+                      setPersonb(personbnew);
+                      const personbpercentnew = personbnew / total;
+                      setPersonbpercent(personbpercentnew);
+                      Evenvalues("personb", personbnew);
                     }
                   }
                 />
@@ -106,10 +196,11 @@ function App() {
                   value={personc}
                   onChange={
                     event => {
-                      const personcnew = parseFloat(event.target.value)
-                      setPersonc(personcnew)
-                      const personcpercentnew = personcnew / total
-                      setPersoncpercent(personcpercentnew)
+                      const personcnew = parseFloat(event.target.value);
+                      setPersonc(personcnew);
+                      const personcpercentnew = personcnew / total;
+                      setPersoncpercent(personcpercentnew);
+                      Evenvalues("personc", personcnew);
                     }
                   }
                 />
@@ -127,10 +218,11 @@ function App() {
                   value={personapercent}
                   onChange={
                     event => {
-                      const personapercentnew = parseFloat(event.target.value)
-                      setPersonapercent(personapercentnew)
-                      const personanum = total * personapercentnew
-                      setPersona(personanum)
+                      const personapercentnew = parseFloat(event.target.value);
+                      setPersonapercent(personapercentnew);
+                      const personanew = total * personapercentnew;
+                      setPersona(personanew);
+                      Evenvalues("persona", personanew);
                     }
                   }
                 />
@@ -146,10 +238,11 @@ function App() {
                   value={personbpercent}
                   onChange={
                     event => {
-                      const personbpercentnew = parseFloat(event.target.value)
-                      setPersonbpercent(personbpercentnew)
-                      const personbnum = total * personbpercentnew
-                      setPersonb(personbnum)
+                      const personbpercentnew = parseFloat(event.target.value);
+                      setPersonbpercent(personbpercentnew);
+                      const personbnew = total * personbpercentnew;
+                      setPersonb(personbnew);
+                      Evenvalues("personb", personbnew);
                     }
                   }
                 />
@@ -165,13 +258,79 @@ function App() {
                   value={personcpercent}
                   onChange={
                     event => {
-                      const personcpercentnew = parseFloat(event.target.value)
-                      setPersoncpercent(personcpercentnew)
-                      const personcnum = total * personcpercentnew
-                      setPersonc(personcnum)
+                      const personcpercentnew = parseFloat(event.target.value);
+                      setPersoncpercent(personcpercentnew);
+                      const personcnew = total * personcpercentnew;
+                      setPersonc(personcnew);
+                      Evenvalues("personc", personcnew);
                     }
                   }
                 />
+              </label>
+            </div>
+            <div className='mb-4 d-flex flex-column'>
+              <label>
+              Lock person A?
+                <button 
+                  className='ml-2'
+                  onClick={
+                    () => {
+                      if ( lock1enabled === true ) {
+                        setlock1enabled(false);
+                        setlock1("lock");
+                      } 
+                      if ( lock1enabled === false ) {
+                        setlock1enabled(true);
+                        setlock1("unlock");
+                      }
+                      setPersonalocked(persona);
+                      setPersonapercentlocked(personapercent);
+                    }
+                  }>
+                  {lock1}
+                </button>
+              </label>
+              <label>
+                Lock person B?
+                  <button 
+                  className='ml-2'
+                  onClick={
+                    () => {
+                      if ( lock2enabled === true ) {
+                        setlock2enabled(false);
+                        setlock2("lock");
+                      } 
+                      if ( lock2enabled === false ) {
+                        setlock2enabled(true);
+                        setlock2("unlock");
+                      }
+                      setPersonblocked(personb);
+                      setPersonbpercentlocked(personbpercent);
+                    }
+                  }>
+                  {lock2}
+                  </button>
+                </label>
+              <label>
+                Lock person C?
+                  <button 
+                  className='ml-2'
+                  onClick={
+                    () => {
+                      if ( lock3enabled === true ) {
+                        setlock3enabled(false);
+                        setlock3("lock");
+                      } 
+                      if ( lock3enabled === false ) {
+                        setlock3enabled(true);
+                        setlock3("unlock");
+                      }
+                      setPersonclocked(personc);
+                      setPersoncpercentlocked(personcpercent);
+                    }
+                  }>
+                  {lock3}
+                  </button>
               </label>
             </div>
             <div>
@@ -179,7 +338,12 @@ function App() {
               <p>...Person b owes ${personb}...</p>
               <p>...Person c owes ${personc}...</p>
               <p>...Which comes to a total of ${total}</p>
-              <p>{ratiotext}</p>
+            </div>
+            <div className='d-inline'>
+              <button id='submit'>
+                submit
+              </button>
+              <p className='text-danger'>{error}</p>
             </div>
           </div>
         </div>
