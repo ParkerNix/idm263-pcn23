dfa9681e6a7b4a63af2b96cc243c496dd46151e7
alliedrake <amd564@drexel.edu>
2023-01-20 18:01:54 -0500

    Added more content/customization to all group page

--- payback/src/pages/allGroups/index.jsx
+++ payback/src/pages/allGroups/index.jsx
@@ -7,27 +7,36 @@ export const allGroups = () => {
             {/* Add back arrow and 3 vertical dots */}
         </div>
 
-        <h4>All Groups</h4>
+        <div className='allGroupsPage'>
 
-        <div className ='groupsContainer'>
-            <div className ='groupsCard'>
-                <div className = 'groupsCardRow'>
-                    <div className ='groupsCardImg'>
-                        {/* <img src="images/groupsCardImg.png" alt="groupImg"> */}
+            <div className='allGroupsPageHeader'>  
+                <h4>All Groups</h4>
+            </div>
+        
+
+            <div className ='groupsContainer'>
+                <div className ='groupsCard'>
+                    <div className = 'groupsCardRow'>
+                        <div className ='groupsCardImg'>
+                            {/* <img src="images/groupsCardImg.png" alt="groupImg"> */}
+                        </div>
+                        <div class='groupsCardInfo'>
+                            <h4>Roomies</h4>
+                            <p>Money money money</p>
+                        </div>
+                        <div className ='groupsCardHeart'>
+                            {/* <img src="images/groupsCardHeart.png" alt="heart"> */}
+                        </div>
                     </div>
-                    <div class='groupsCardInfo'>
-                        <h4>Roomies</h4>
-                        <p>Money money money</p>
-                    </div>
-                    <div className ='groupsCardHeart'>
-                        {/* <img src="images/groupsCardHeart.png" alt="heart"> */}
-                    </div>
-                </div>
-                <div className ='groupsCardRow'>
-                    <div className ='groupsCardIcons'>
-                        {/* <img src="">
-                        <img src="">
-                        <img src=""> */}
+                    <div className ='groupsCardRowTwo'>
+                        <div class="groupsCardIcons">
+                            {/* <img id="smileySmiley" src="images/smiley_smiley.png">
+                            <img id="molynaSmiley" src="images/molyna_smiley.png">
+                            <img id="parkerSmiley" src="images/parker_smiley.png"> */}
+                        </div>
+                        <div class="groupsCardButton">
+                        <button>View Group</button>
+                        </div>
                     </div>
                 </div>
             </div>
